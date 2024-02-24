98096288eea97cf17db245df5d5e54609608a58e
George Ryan <80276959+ThatsCrazy4171@users.noreply.github.com>
2024-02-24 15:29:06 -0500

    Almost working quiz and vid upload.

--- routers/vids.py
+++ routers/vids.py
@@ -1,6 +1,6 @@
 import os
 from typing import List, Optional
-from fastapi import APIRouter, Cookie, HTTPException, Request, status
+from fastapi import APIRouter, Cookie, HTTPException, Request, UploadFile, status
 from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import ConfigDict, BaseModel, Field
@@ -10,6 +10,8 @@ from typing_extensions import Annotated
 from bson import ObjectId
 import motor.motor_asyncio
 from pymongo import ReturnDocument, errors
+from pathlib import Path
+import uuid
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
@@ -18,16 +20,7 @@ client = motor.motor_asyncio.AsyncIOMotorClient("127.0.0.1:27017")
 db = client.train
 
 user_collection = db.get_collection("users")
-vid_collection = db.get_collection("vids")
-# Represents an ObjectId field in the database.
-# It will be represented as a `str` on the model so that it can be serialized to JSON.
-PyObjectId = Annotated[str, BeforeValidator(str)]
 
-class VidModel(BaseModel):
-   id: Optional[PyObjectId] = Field(alias="_id", default=None)
-   file_name: str = Field(...)
-   quiz: str = Field(...)
-   model_config = ConfigDict(populate_by_name=True, arbitrary_types_allowed=True)
 
 async def get_videos():
     videos = []
@@ -53,3 +46,43 @@ async def get_video(request: Request, id: str, user: Annotated[str | None, Cooki
             raise HTTPException(403, "Not assigned content.")
     else:
         raise HTTPException(403, "Not allowed to view resource.")
+
+@router.post("/upload_quiz", response_description="Upload a quiz file.", status_code=status.HTTP_202_ACCEPTED)
+async def upload_quiz(quiz: UploadFile):
+    file_content = await quiz.read()
+
+    quiz_file = open(f"{quiz.filename}", "w")
+    quiz_file.write(file_content.decode("utf-8"))
+
+    return {"filename": quiz.filename, "contents": file_content}
+
+@router.post("/upload_video", response_description="Upload a video", status_code=status.HTTP_202_ACCEPTED)
+async def upload_video(video: UploadFile):
+    file_content = await video.read()
+
+    print(video.file)
+    video_file = open(f"{video.filename}", "w+b")
+    video_file.write(file_content)
+
+    return {"filename": video.filename, "contents": file_content}
+
+@router.post("/upload_qv", response_description="Upload quiz and video file.", status_code=status.HTTP_202_ACCEPTED)
+async def upload_video_and_quiz(files: list[UploadFile]):
+    if len(files) == 2:
+        id = str(uuid.uuid4())
+        path = Path(f"./vids/{id}")
+        path.mkdir(parents=True, exist_ok=True)
+        for file in files:
+            if ".mp4" in file.filename:
+                file_content = await file.read()
+                video_file = open(f"{path}/{file.filename}", "w+b")
+                video_file.write(file_content)
+            elif ".txt" in file.filename:
+                file_content = await file.read()
+                quiz_file = open(f"{path}/{file.filename}", "w")
+                quiz_file.write(file_content.decode("utf-8"))
+            else:
+                return {"message": "Must be .txt and .mp4!"}
+    else:
+        return {"message": "Must upload one .mp4 and one .txt!"}
+    return {"filenames": [file.filename for file in files]}
