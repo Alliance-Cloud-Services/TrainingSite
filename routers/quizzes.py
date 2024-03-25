d6eb401f04e02f0b64505366602d36b125016d50
ThatsCrazy4171 <80276959+ThatsCrazy4171@users.noreply.github.com>
2024-03-25 13:32:38 -0400

    Added Basic Quiz Class

---
+++ routers/quizzes.py
@@ -0,0 +1,66 @@
+import re
+class Question:
+    def __init__(self, text: str, options: list[str], correct_answer: str):
+        self.text = text
+        self.correct_answer = correct_answer
+        self.options = options
+    def check_answer(self, answer: str):
+        if answer == self.correct_answer and answer in self.options:
+            return True
+        else:
+            return False
+        
+class Quiz:
+    def __init__(self, questions: list[Question] = None, score = 0, file = None):
+        self.questions = questions
+        self.score = score
+    def score_quiz(self, answers: list[str]):
+        correct_count = 0
+        quiz_len = len(self.questions)
+
+        for question in self.questions:
+            #print(question.text)
+            for answer in answers:
+                # print(answer)
+                # print(question.correct_answer.replace("Answer: ", ""))
+                if answer in question.correct_answer.replace("Answer: ", ""):
+                    # print("Correct!")
+                    correct_count +=1
+                else:
+                    continue
+        # print(correct_count)
+        # print(quiz_len)
+        score = (correct_count/quiz_len)*100
+        return score
+    def from_file(self, file):
+        self.questions = []
+        with open(file, "r") as quiz_file:
+            file_contents = quiz_file.readlines()
+            # file_split = re.split("\n", file_contents)
+            # file_string = ''.join(file_split)
+
+            
+            question = Question(None, [], None)
+            for line in file_contents:
+                
+                # print(line)
+                question_search = re.search(r"Question:", line)
+                answer_search = re.search(r"Answer:", line)
+                option_search = re.search(r"Option:", line)
+
+                if question_search:
+                    question.text = question_search.string.replace("Question:", "")
+                if option_search:
+                    question.options.append(option_search.string.replace("Option:", "").replace("\n", ""))
+                if answer_search:
+                    question.correct_answer = answer_search.string.replace("Answer:", "")
+                
+                if line == "\n":
+                    self.questions.append(question)
+                    question = Question(None, [], None)
+                    
+
+    
+
+
+
